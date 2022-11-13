1e24a40e3202e66f018324a2b04175e9a67d70a4
Svetlin Mollov <s.s.mollov@gmail.com>
2022-11-13 23:26:11 +0200

    chore: add tests

--- test/features/app_init/presenters/app_init_presenter_test.dart
+++ test/features/app_init/presenters/app_init_presenter_test.dart
@@ -27,6 +27,7 @@ void main() {
         Stream.fromIterable([const User.anonymous()]),
       );
       when(() => AppInitMocks.appInitUseCase.execute()).thenAnswer((_) => successFuture(unit));
+      when(() => navigator.openLogin(any())).thenAnswer((_) => Future.value());
 
       // WHEN
       await presenter.onInit();
@@ -52,6 +53,25 @@ void main() {
 
       // THEN
       verify(() => navigator.showError(any()));
+    },
+  );
+
+  test(
+    'should open login when appInitUseCase succeeds',
+    () async {
+      // GIVEN
+      whenListen(
+        Mocks.userStore,
+        Stream.fromIterable([const User.anonymous()]),
+      );
+      when(() => AppInitMocks.appInitUseCase.execute()).thenAnswer((_) => failFuture(const AppInitFailure.unknown()));
+      when(() => navigator.openLogin(any())).thenAnswer((_) => Future.value());
+
+      // WHEN
+      await presenter.onInit();
+
+      // THEN
+      verify(() => navigator.openLogin(any()));
     },
   );
 
--- test/features/auth/presenters/login_presenter_test.dart
+++ test/features/auth/presenters/login_presenter_test.dart
@@ -1,28 +1,66 @@
+import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
+import 'package:flutter_demo/features/auth/domain/use_cases/validate_credentials_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_initial_params.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
 import 'package:flutter_demo/features/auth/login/login_presenter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../mocks/auth_mock_definitions.dart';
+import '../mocks/auth_mocks.dart';
 
 void main() {
   late LoginPresentationModel model;
   late LoginPresenter presenter;
   late MockLoginNavigator navigator;
+  late ValidateCredentialsUseCase validateCredentialsUseCase;
+  late LogInUseCase logInUseCase;
 
   test(
-    'sample test',
+    'GIVEN username is empty THEN isLoginEnabled is false',
     () {
-      expect(presenter, isNotNull); // TODO implement this
+      // GIVEN
+      presenter.onUsernameChanged('');
+      presenter.onPasswordChanged('password');
+
+      // THEN
+      expect(presenter.state.isLoginEnabled, isFalse);
+    },
+  );
+
+  test(
+    'GIVEN password is empty THEN isLoginEnabled is false',
+    () {
+      // GIVEN
+      presenter.onUsernameChanged('username');
+      presenter.onPasswordChanged('');
+
+      // THEN
+      expect(presenter.state.isLoginEnabled, isFalse);
+    },
+  );
+
+  test(
+    'GIVEN username and password are non-empty THEN isLoginEnabled is true',
+    () {
+      // GIVEN
+      presenter.onUsernameChanged('username');
+      presenter.onPasswordChanged('password');
+
+      // THEN
+      expect(presenter.state.isLoginEnabled, isTrue);
     },
   );
 
   setUp(() {
     model = LoginPresentationModel.initial(const LoginInitialParams());
     navigator = MockLoginNavigator();
+    validateCredentialsUseCase = const ValidateCredentialsUseCase();
+    logInUseCase = AuthMocks.logInUseCase;
     presenter = LoginPresenter(
       model,
       navigator,
+      validateCredentialsUseCase,
+      logInUseCase,
     );
   });
 }
